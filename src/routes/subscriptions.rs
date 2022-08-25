75a601a2dfc8a1c140b2466bfadb5105da55bd90
Miguel Santos Correa <miguelsantoscorrea@gmail.com>
2022-08-25 18:54:01 +0200

    add anyhow to confirm endpoint

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,7 @@ use crate::{
     domain::{NewSubscriber, SubscriberEmail, SubscriberName},
     email_client::EmailClient,
     startup::ApplicationBaseUrl,
+    utils::error_chain_fmt,
 };
 
 #[derive(serde::Deserialize)]
@@ -91,19 +92,6 @@ pub async fn store_token(
     .execute(transaction)
     .await
     .map_err(|e| StoreTokenError(e))?;
-    Ok(())
-}
-
-fn error_chain_fmt(
-    e: &impl std::error::Error,
-    f: &mut std::fmt::Formatter<'_>,
-) -> std::fmt::Result {
-    writeln!(f, "{}\n", e)?;
-    let mut current = e.source();
-    while let Some(cause) = current {
-        writeln!(f, "Caused by:\n\t{}", cause)?;
-        current = cause.source();
-    }
     Ok(())
 }
 
